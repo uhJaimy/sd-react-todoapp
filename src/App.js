99f1da3986249fb8cc7f1d2b7ae4c119057e04d5
uhJaimy <58993292+uhJaimy@users.noreply.github.com>
2024-10-21 11:33:26 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import deletebutton from './delete.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="actions">
+        <div class="add">
+          <textarea placeholder='Task Name'></textarea>
+          <textarea placeholder='Task Description'></textarea>
+          <button>Add Task</button>
+        </div>
+        <button>Clear All</button>
+      </div>
+      <div class="categories">
+        <div class="tasks high">
+          <h2>High priority tasks</h2>
+          <div class="task">
+            <h3>Task Name</h3>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut est quis libero blandit suscipit at iaculis leo. Nunc dapibus enim ac sem feugiat imperdiet. Suspendisse potenti. Mauris eu pellentesque diam. Sed nec nibh leo. Sed vulputate eu purus eget gravida. Phasellus pretium nisi interdum nunc vulputate feugiat.</p>
+          </div>
+        </div>
+        <div class="tasks normal">
+          <h2>Regular tasks</h2>
+          <div class="task">
+            <h3>Task Name</h3>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut est quis libero blandit suscipit at iaculis leo. Nunc dapibus enim ac sem feugiat imperdiet. Suspendisse potenti. Mauris eu pellentesque diam. Sed nec nibh leo. Sed vulputate eu purus eget gravida. Phasellus pretium nisi interdum nunc vulputate feugiat.</p>
+          </div>
+        </div>
+        <div class="tasks completed">
+          <h2>Completed tasks</h2>
+          <div class="task">
+            <h3>Task Name</h3>
+            <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut est quis libero blandit suscipit at iaculis leo. Nunc dapibus enim ac sem feugiat imperdiet. Suspendisse potenti. Mauris eu pellentesque diam. Sed nec nibh leo. Sed vulputate eu purus eget gravida. Phasellus pretium nisi interdum nunc vulputate feugiat.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
